b3ec0e9fc455d463cfe6eb3f918cbee74031b12c
teregudi <teregudi.tamashi@gmail.com>
2021-11-04 13:59:58 +0100

    first commit

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using WPF_App.MVVM.Model;
+
+namespace WPF_App.MVVM.ViewModel {
+   class MainViewModel {
+
+      public ObservableCollection<MessageModel> Messages { get; set; }
+      public ObservableCollection<ContactModel> Contacts { get; set; }
+
+      public MainViewModel() {
+         Messages = new ObservableCollection<MessageModel>();
+         Contacts = new ObservableCollection<ContactModel>();
+      }
+   }
+}
