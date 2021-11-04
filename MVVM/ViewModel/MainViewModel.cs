76fc1dd5488e1a235bbe888c9ab73b5ee15de28e
teregudi <teregudi.tamashi@gmail.com>
2021-11-04 16:07:12 +0100

    list contacts on left panel

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -13,6 +13,72 @@ namespace WPF_App.MVVM.ViewModel {
       public MainViewModel() {
          Messages = new ObservableCollection<MessageModel>();
          Contacts = new ObservableCollection<ContactModel>();
+
+         Messages.Add(new MessageModel {
+            Username = "Ezerszatyrú Piri",
+            UsernameColor = "#4010ff",
+            ImageSource = "https://www.arlette.hu/wp-content/uploads/2017/02/%C3%B6regasszony-600x460.jpg",
+            Message = "Segítene átkelni a zebrán, fiatalember?",
+            Time = DateTime.Now,
+            IsNativeOrigin = false,
+            FirstMessage = true
+         });
+
+         Messages.Add(new MessageModel {
+            Username = "Ezerszatyrú Piri",
+            UsernameColor = "#4010ff",
+            ImageSource = "https://www.arlette.hu/wp-content/uploads/2017/02/%C3%B6regasszony-600x460.jpg",
+            Message = "Döntsön gyorsan, nagyon sietek.",
+            Time = DateTime.Now,
+            IsNativeOrigin = false,
+            FirstMessage = false
+         });
+
+         Messages.Add(new MessageModel {
+            Username = "Ezerszatyrú Piri",
+            UsernameColor = "#4010ff",
+            ImageSource = "https://www.arlette.hu/wp-content/uploads/2017/02/%C3%B6regasszony-600x460.jpg",
+            Message = "A banyatankomat is elhúzhatná a Penny-ig.",
+            Time = DateTime.Now,
+            IsNativeOrigin = false,
+            FirstMessage = false
+         });
+
+         Messages.Add(new MessageModel {
+            Username = "Dagadt Pista",
+            UsernameColor = "#4010ff",
+            ImageSource = "https://ripost.hu/wp-content/uploads/kepadatbazis/thumbs/2015-11-05/1c36163323a700cd8b78780b0480fa78/620x0.jpg",
+            Message = "Majd én segítek, Piri néni.",
+            Time = DateTime.Now,
+            IsNativeOrigin = false
+         });
+
+         Messages.Add(new MessageModel {
+            Username = "Dagadt Pista",
+            UsernameColor = "#4010ff",
+            ImageSource = "https://ripost.hu/wp-content/uploads/kepadatbazis/thumbs/2015-11-05/1c36163323a700cd8b78780b0480fa78/620x0.jpg",
+            Message = "Ezek a mai fiatalok semmire nem jók.",
+            Time = DateTime.Now,
+            IsNativeOrigin = false
+         });
+
+         Contacts.Add(new ContactModel {
+            Username = "Ezerszatyrú Piri",
+            ImageSource = "https://www.arlette.hu/wp-content/uploads/2017/02/%C3%B6regasszony-600x460.jpg",
+            Messages = Messages
+         });
+
+         Contacts.Add(new ContactModel {
+            Username = "Dagadt Pista",
+            ImageSource = "https://ripost.hu/wp-content/uploads/kepadatbazis/thumbs/2015-11-05/1c36163323a700cd8b78780b0480fa78/620x0.jpg",
+            Messages = Messages
+         });
+
+         Contacts.Add(new ContactModel {
+            Username = "Rikárdó",
+            ImageSource = "https://i.ytimg.com/vi/BP3lODEM_2I/hqdefault.jpg",
+            Messages = Messages
+         });
       }
    }
 }
