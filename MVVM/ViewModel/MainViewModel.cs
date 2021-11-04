e5e6b66bd5dc28a82ed1a20bc51e27ab28c712e1
teregudi <teregudi.tamashi@gmail.com>
2021-11-04 19:32:59 +0100

    almost done

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using WPF_App.Core;
 using WPF_App.MVVM.Model;
 
 namespace WPF_App.MVVM.ViewModel {
-   class MainViewModel {
+   class MainViewModel : ObservableObject {
 
       public ObservableCollection<MessageModel> Messages { get; set; }
       public ObservableCollection<ContactModel> Contacts { get; set; }
+
+      public ContactModel SelectedContact { get; set; }
+      private string _message
+         ;
+
+      public string Message {
+         get { return _message; }
+         set { _message = value;
+            OnPropertyChanged();
+         }
+      }
+
 
       public MainViewModel() {
          Messages = new ObservableCollection<MessageModel>();
